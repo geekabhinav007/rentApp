7db1e6254b916eb6dbc2767c3fac68af023e6889
geekabhinav007 <abhinav26january@gmail.com>
2024-05-01 23:26:07 +0530

    Footer text added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,11 +11,9 @@ function Footer() {
                     </a>
                     <div class="mt-6 lg:max-w-xl">
                         <p class="text-sm text-gray-800">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi felis mi, faucibus dignissim lorem
-                            id, imperdiet interdum mauris. Vestibulum ultrices sed libero non porta. Vivamus malesuada urna eu
-                            nibh malesuada, non finibus massa laoreet. Nunc nisi velit, feugiat a semper quis, pulvinar id
-                            libero. Vivamus mi diam, consectetur non orci ut, tincidunt pretium justo. In vehicula porta
-                            molestie. Suspendisse potenti.
+                            Xingoda serves as a comprehensive rental solution, amalgamating both user and vendor rentals onto a single platform to facilitate a smooth and efficient rental process.
+                            User Rentals: Everyday individuals can leverage our platform to rent out items lying idle at home, providing them with an opportunity to earn extra income.
+                            Vendor Rentals: Our platform serves as a central hub, aggregating various rental vendors. This approach not only expands the customer base for vendors but also ensures a seamless rental experience for customers.
                         </p>
                     </div>
                 </div>
@@ -49,22 +47,22 @@ function Footer() {
 
             </div>
             <div class="flex justify-center space-x-5 lg:justify-center border-t pt-5 pb-5">
-    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-        <img src="https://img.icons8.com/fluent/30/000000/facebook-new.png" />
-    </a>
-    <a href="https://www.linkedin.com/company/xingodaxingoda/" target="_blank" rel="noopener noreferrer">
-        <img src="https://img.icons8.com/fluent/30/000000/linkedin-2.png" />
-    </a>
-    <a href="https://www.instagram.com/xingoda/?igsh=MTM3bXk3N2N3Nm10aQ" target="_blank" rel="noopener noreferrer">
-        <img src="https://img.icons8.com/fluent/30/000000/instagram-new.png" />
-    </a>
-    <a href="https://messenger.com" target="_blank" rel="noopener noreferrer">
-        <img src="https://img.icons8.com/fluent/30/000000/facebook-messenger--v2.png" />
-    </a>
-    <a href="https://twitter.com/xingoda?t=bRnq9l6heChLc--hh_q63A&s=09" target="_blank" rel="noopener noreferrer">
-        <img src="https://img.icons8.com/fluent/30/000000/twitter.png" />
-    </a>
-</div>
+                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                    <img src="https://img.icons8.com/fluent/30/000000/facebook-new.png" />
+                </a>
+                <a href="https://www.linkedin.com/company/xingodaxingoda/" target="_blank" rel="noopener noreferrer">
+                    <img src="https://img.icons8.com/fluent/30/000000/linkedin-2.png" />
+                </a>
+                <a href="https://www.instagram.com/xingoda/?igsh=MTM3bXk3N2N3Nm10aQ" target="_blank" rel="noopener noreferrer">
+                    <img src="https://img.icons8.com/fluent/30/000000/instagram-new.png" />
+                </a>
+                <a href="https://messenger.com" target="_blank" rel="noopener noreferrer">
+                    <img src="https://img.icons8.com/fluent/30/000000/facebook-messenger--v2.png" />
+                </a>
+                <a href="https://twitter.com/xingoda?t=bRnq9l6heChLc--hh_q63A&s=09" target="_blank" rel="noopener noreferrer">
+                    <img src="https://img.icons8.com/fluent/30/000000/twitter.png" />
+                </a>
+            </div>
 
             <div class="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
                 <p class="text-sm text-gray-600">© Copyright 2004-2025 Xingoda. All rights reserved.</p>
