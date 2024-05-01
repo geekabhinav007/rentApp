6972b0aca89c2abe2faff73df0af43b050efc168
geekabhinav007 <abhinav26january@gmail.com>
2024-05-02 00:14:55 +0530

    fixed hero button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -15,7 +15,7 @@ function Hero() {
 
 </p>
             <div className="mt-8 flex w-full space-x-8 justify-center"><a
-                href="/exercises"><button className="inline-flex items-center justify-center px-4 py-2.5 border border-transparent text-sm font-medium rounded-md shadow-sm focus:outline-none ring-2 ring-offset-2 ring-transparent ring-offset-transparent disabled:bg-gray-400 appearance-none text-white bg-lime-600 hover:bg-lime-700 focus:ring-lime-500 focus:ring-offset-white !px-12 !shadow-lg !rounded-full !text-base"><p>Start now for free</p></button></a>
+                href="#"><button className="inline-flex items-center justify-center px-4 py-2.5 border border-transparent text-sm font-medium rounded-md shadow-sm focus:outline-none ring-2 ring-offset-2 ring-transparent ring-offset-transparent disabled:bg-gray-400 appearance-none text-white bg-lime-600 hover:bg-lime-700 focus:ring-lime-500 focus:ring-offset-white !px-12 !shadow-lg !rounded-full !text-base"><p>Start now for free</p></button></a>
             </div>
         </div>
     </div>
