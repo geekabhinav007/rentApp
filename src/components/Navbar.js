1d3767fdc1067ebcb5810a2be7c1f1ba225fff50
geekabhinav007 <abhinav26january@gmail.com>
2024-05-01 22:46:11 +0530

    added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import logo from '../XingodaLogo.svg'
 
 function Navbar() {
     return (
@@ -6,7 +7,7 @@ function Navbar() {
         <>        <div className="mx-auto max-w-screen-xl px-6 lg:px-8 relative">
             <div className="relative flex h-16 space-x-10 w-full">
                 <div className="flex justify-start"><a className="flex flex-shrink-0 items-center" href="/">
-                    <img className="block h-8 w-auto" height="32" src="https://www.svgrepo.com/show/303650/neo-logo.svg" />
+                    <img className="block h-8 w-auto" height="32" src={logo} />
                 </a>
                 </div>
                 <div className="flex-shrink-0 flex px-2 py-3 items-center space-x-8 flex-1 justify-end justify-self-end "><a
