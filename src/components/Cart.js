7b9636592b2692123578ac4dbd69f46a46f91e36
geekabhinav007 <abhinav26january@gmail.com>
2024-05-06 18:36:11 +0530

    cart responsive done

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -69,14 +69,14 @@ function Cart() {
 
 
   return (
-    <div className='rounded-xl p-10 mx-10 mt-20 mb-20 bg-gray-100'>
-      <h2 className='text-2xl font-bold mb-4'>Cart</h2>
+    <div className='rounded-xl p-4 sm:p-10 mx-4 sm:mx-10 mt-10 mb-10 bg-gray-100'>
+      <h2 className='text-xl sm:text-2xl font-bold mb-4'>Cart</h2>
       {cartItems.length > 0 ? (
         <ul>
           {cartItems.map((item, index) => (
-            <li key={index} className='flex items-center justify-between bg-white p-4 mb-2 rounded shadow'>
-              <img className='w-16 h-16 object-cover rounded' src={item.image} alt={item.name} />
-              <div className='ml-4'>
+            <li key={index} className='flex flex-col sm:flex-row items-start sm:items-center justify-between bg-white p-4 mb-2 rounded shadow'>
+              <img className='w-24 h-24 sm:w-16 sm:h-16 object-cover rounded' src={item.image} alt={item.name} />
+              <div className='mt-4 sm:mt-0 sm:ml-4'>
                 <h3 className='text-lg font-medium'>{item.name}</h3>
                 <p className='text-sm text-gray-600'>{item.description}</p>
                 <p><strong className='text-green-700'>Selected Plan:</strong> {item.selectedPlan}</p>
@@ -87,9 +87,7 @@ function Cart() {
                 </div>
                 <p><strong className='text-green-700'>Security Deposit:</strong> ₹{item.securityDeposit}</p>
               </div>
-
               <button className=" bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-4 rounded m-2" onClick={() => handleRemoveItem(item.id, item.uid)}>Remove from Cart</button>
-
             </li>
           ))}
         </ul>
@@ -100,6 +98,7 @@ function Cart() {
       <button className=" bg-green-700 hover:bg-green-800 text-white font-bold py-2 px-4 rounded m-2" onClick={() => handlePlaceOrder(totalPrice)} disabled={Object.keys(selectedPlan).length !== cartItems.length}>Place Your Order</button>
     </div>
   );
+  
 }
 
 export default Cart;
